b537bf1a6ebfd5aed37d4bb5ea24e49c3ef22d10
Руслан Русланов <sharp_blade666@mail.ru>
2022-07-17 19:16:01 +0300

    Regisrers test settings

--- Distributive/TurnMill/Sinumerik (840D) TurnMill_DN/Postprocessor.cs
+++ Distributive/TurnMill/Sinumerik (840D) TurnMill_DN/Postprocessor.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 namespace SprutTechnology.SCPostprocessor
 {
+    enum OpType {
+        Unknown,
+        Mill,
+        Turning, //токарная обработка
+        Lathe,
+        Auxiliary,
+        WireEDM
+    }
 
     public partial class NCFile: TTextNCFile
     {
         ///<summary>Main nc-programm number</summary>
         public int ProgNumber {get; set;}
+
+        ///<summary>Last point (X, Y, Z) was written to the nc-file</summary>
+        public TInp3DPoint LastP {get; set;}
 
         public override void OnInit()
         {
@@ -31,8 +42,17 @@ namespace SprutTechnology.SCPostprocessor
     {
         #region Common variables definition
 
-        ///<summary>Current nc-file</summary>
-        NCFile nc;
+        ///<summary>Current nc-file. It could be main or sub.</summary>
+        public NCFile nc;
+
+        ///<summary>G81-G89 cycle is on</summary>
+        bool cycleIsOn = false;
+
+        ///<summary>X axis scale coefficient (1 - radial, 2 - diametral)</summary>
+        double xScale = 1.0;
+
+        ///<summary>Type of current operation (mill, lathe, etc.)</summary>
+        OpType currentOperationType = OpType.Unknown;
  
         #endregion
 
@@ -63,8 +83,8 @@ namespace SprutTechnology.SCPostprocessor
             nc.WriteLine("O" + Str(nc.ProgNumber));
 
             PrintAllTools();
-            nc.Block.Show(nc.BlockN, nc.GWCS, nc.GInterp);
-            nc.Block.Out();
+            //nc.Block.Show(nc.BlockN, nc.GWCS, nc.GInterp);
+            // nc.Block.Out();
         }
 
         public override void OnFinishProject(ICLDProject prj)
@@ -84,11 +104,71 @@ namespace SprutTechnology.SCPostprocessor
             nc.WriteLine("( " + cmd.CLDataS + " )");
         }
 
+        public override void OnOrigin(ICLDOriginCommand cmd, CLDArray cld)
+        {
+            base.OnOrigin(cmd, cld);
+        }
+
+        //(ORIGIN)
         public override void OnWorkpieceCS(ICLDOriginCommand cmd, CLDArray cld)
         {
             nc.GWCS.v = cmd.CSNumber;
+            nc.Block.Show(nc.BlockN, nc.GWCS);
+            nc.Block.Out();
+
+            nc.T.v = cmd.TechOperation.Tool.Number;
+            nc.Block.Show(nc.BlockN, nc.T);
+            nc.Block.Out();
+        }
+        
+        public override void OnRapid(ICLDRapidCommand cmd, CLDArray cld)
+        {
+            base.OnRapid(cmd, cld);
         }
 
+        //(AbsMov)
+        public override void OnGoto(ICLDGotoCommand cmd, CLDArray cld)
+        {
+            //я не шарю, углы в радианах или в градусах в cld
+            Func<double,double> cosecant = (c => (1 / Math.Sin(c)));
+            Func<double,double> secant = (d => (1 / Math.Cos(d)));
+
+            if (nc.GInterp.v > 1 && nc.GInterp.v < 4) nc.GInterp.v = 1;
+            if (nc.GInterp.v == 33) nc.Block.Show(nc.F, nc.GInterp);
+            if (nc.GPolarOrCyl.v == 1)
+            {
+                nc.X.v = cmd.EP.X * cosecant(nc.C.v) - cmd.EP.Y * secant(nc.C.v);
+                nc.Y.v = cmd.EP.Y * cosecant(nc.C.v) + cmd.EP.X * secant(nc.C.v);
+            }
+
+            else
+            {
+                nc.X.v = cmd.EP.X * xScale; //xScale в этом случае должно быть установлено = 2
+                nc.Y.v = cmd.EP.Y;
+            }
+
+            nc.Z.v = cmd.EP.Z;
+            if(currentOperationType == OpType.Turning )
+            {
+                nc.X.v = cmd.EP.Y;
+            }
+            
+            nc.Block.Out();
+            nc.LastP = cmd.EP;
+            // if (nc.GInterp.v==32) {
+            //     nc.Block.Show(nc.F, nc.GInterp);
+            // } else if (nc.GInterp > 1)
+            //     nc.GInterp.v = 1;
+            // nc.X.v = xScale*cmd.EP.X;
+            // nc.Y.v = cmd.EP.Y;
+            // nc.Z.v = cmd.EP.Z;
+            // if (!cycleIsOn) {
+            //     nc.Block.Out();
+            // }
+            // nc.LastP = cmd.EP;
+        }
+
+        //(RAPID)
         public override void OnMoveVelocity(ICLDMoveVelocityCommand cmd, CLDArray cld)
         {
             if (cmd.IsRapid) {
@@ -98,16 +178,6 @@ namespace SprutTechnology.SCPostprocessor
                     nc.GInterp.v = 1;
                 nc.F.v = cmd.FeedValue;
             }
-        }
-
-        public override void OnGoto(ICLDGotoCommand cmd, CLDArray cld)
-        {
-            if (nc.GInterp > 1)
-                nc.GInterp.v = 1;
-            nc.X.v = cmd.EP.X;
-            nc.Y.v = cmd.EP.Y;
-            nc.Z.v = cmd.EP.Z;
-            nc.Block.Out();
         }
 
         public override void OnMultiGoto(ICLDMultiGotoCommand cmd, CLDArray cld)
